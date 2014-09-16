79a0301dd55033eb5962f8f160d3e2d14a5f7262
Michael Toth <mt.toth@gmail.com>
2014-09-15 23:40:27 -0400

    Code for quick find algorithm.  Need to test tomorrow

--- Percolation/percolation.js
+++ Percolation/percolation.js
@@ -19,9 +19,43 @@ function draw() {
     
 }
 
-function percolation() {
-    var size;
+// Declare namespace:
+var Percolation = new function() {
 
 }
 
+// Declare weighted quick union union-find namespace:
+function WeightedQuickUnionUF(N) {
+    
+    // Constructor
+    var id = [];
+    var sz = []; 
+    this.count = N;
+    for (i=0; i<N; i++) {
+        id[i] = i; // id[i] = parent of i
+        sz[i] = 1; // sz[i] = number of objects in subtree rooted at i
+    }
+
+    // Returns the component id for the component containing site
+    this.find = function(p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p
+    }
+
+    this.union = function(p, q) {
+        var rootP = find(p);
+        var rootQ = find(q);
+        if (rootP == rootQ) { return; }
+        
+        // make smaller root point to large one
+        if (sz[rootP] < sz[rootQ]) {
+            id[rootP] = rootQ; sz[rootQ] += sz[rootP];
+        } else {
+            id[rootQ] = rootP; sz[rootP] += sz[rootQ];
+        }
+    }
+
+}
 
